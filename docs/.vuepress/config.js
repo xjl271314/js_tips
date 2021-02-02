bfc44441d9268bf15e39adab642ec84648d38432
许将龙 <xujl@weipaitang.com>
2021-02-02 20:43:12 +0800

    fix-url

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,74 +1,74 @@
 const moment = require('moment');
 
 module.exports = {
-  title: 'JavaScript Tools',
-  description: '常用的js方法工具',
-  dest: './dist',
-  base: '/js_tips/',
-  repo: '',
-  head: [
-    ['link', { rel: 'icon', href: `/images/favicon.png` }],
-    ["link", { rel: "stylesheet", href: "https://cdn.bootcdn.net/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" }],
-    ['meta', { name: 'theme-color', content: '#00adb5' }],
-    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-    ['meta', { name: 'msapplication-TileColor', content: '#00adb5' }],
-    ['meta', {name:'description', itemprop: 'description', content: '常用的js方法，js_tips' }],
-    ['meta', { itemprop: 'name', content: 'js_tips' }],
-    ['meta', { itemprop: 'image', content: '/js_tips/images/favicon.png' }],
-  ],
-  markdown: {
-    anchor: { permalink: false },
-    toc: { includeLevel: [1, 2] },
-    config: md => {
-      md.use(require('markdown-it-include'), './')
+    title: 'JavaScript Tools',
+    description: '常用的js方法工具',
+    dest: './dist',
+    base: '/js_tips/',
+    repo: '',
+    head: [
+        ['link', { rel: 'icon', href: `/images/favicon.png` }],
+        ["link", { rel: "stylesheet", href: "https://cdn.bootcdn.net/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" }],
+        ['meta', { name: 'theme-color', content: '#00adb5' }],
+        ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+        ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+        ['meta', { name: 'msapplication-TileColor', content: '#00adb5' }],
+        ['meta', { name: 'description', itemprop: 'description', content: '常用的js方法，js_tips' }],
+        ['meta', { itemprop: 'name', content: 'js_tips' }],
+        ['meta', { itemprop: 'image', content: '/js_tips/images/favicon.png' }],
+    ],
+    markdown: {
+        anchor: { permalink: false },
+        toc: { includeLevel: [1, 2] },
+        config: md => {
+            md.use(require('markdown-it-include'), './')
+        }
+    },
+    plugins: [
+        require('./plugins/copy/index.js'),
+        '@vuepress/back-to-top',
+        '@vuepress/active-header-links',
+        ['@vuepress/last-updated', {
+            transformer: (timestamp, lang) => {
+                moment.locale(lang)
+                return moment(timestamp).format('YYYY-MM-DD HH:mm:SS')
+            }
+        }],
+        ['@vuepress/register-components'],
+    ],
+    themeConfig: {
+        nav: [
+            { text: 'css tricks', link: 'https://xjl271314.github.io/css_tips' },
+            { text: 'blog', link: 'https://xjl271314.github.io/docs' },
+            { text: 'GitHub', link: 'https://github.com/xjl271314/js-tips' },
+        ],
+        sidebar: [
+            '/dom/',
+            '/bom/',
+            '/type/',
+            '/array/',
+            '/string/',
+            '/object/',
+            '/function/',
+            '/date/',
+            '/number/',
+            '/cookie/',
+            '/copy/',
+            '/script/',
+            '/styles/',
+            '/file/',
+            '/other/'
+        ],
+        lastUpdated: '上次更新时间',
+        // 假定 GitHub。也可以是一个完整的 GitLab 网址
+        repo: 'https://github.com/xjl271314/js-tips',
+        // 如果你的文档不在仓库的根部
+        // docsDir: 'docs',
+        // 可选，默认为 master
+        docsBranch: 'master',
+        // 默认为 true，设置为 false 来禁用
+        editLinks: false,
+        smoothScroll: true
     }
-  },
-  plugins: [
-    require('./plugins/copy/index.js'),
-    '@vuepress/back-to-top',
-    '@vuepress/active-header-links',
-    ['@vuepress/last-updated',{
-      transformer: (timestamp, lang) => {
-        moment.locale(lang)
-        return moment(timestamp).format('YYYY-MM-DD HH:mm:SS')
-      }
-    }],
-    ['@vuepress/register-components'],
-  ],
-  themeConfig: {
-    nav: [
-      { text: 'css tricks', link: '' },
-      { text: 'blog', link: 'https://xjl271314.github.io/docs' },
-      { text: 'GitHub', link: 'https://github.com/xjl271314/js-tips' },
-    ],
-    sidebar: [
-      '/dom/',
-      '/bom/',
-      '/type/',
-      '/array/',
-      '/string/',
-      '/object/',
-      '/function/',
-      '/date/',
-      '/number/',
-      '/cookie/',
-      '/copy/',
-      '/script/',
-      '/styles/',
-      '/file/',
-      '/other/'
-    ],
-    lastUpdated: '上次更新时间' ,
-    // 假定 GitHub。也可以是一个完整的 GitLab 网址
-    repo: 'https://github.com/xjl271314/js-tips',
-    // 如果你的文档不在仓库的根部
-    // docsDir: 'docs',
-    // 可选，默认为 master
-    docsBranch: 'master',
-    // 默认为 true，设置为 false 来禁用
-    editLinks: false,
-    smoothScroll: true
-  }
 }
 
